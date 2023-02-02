bdfec56067725ad36eef93382e0c7ba6dbf4f1fd
Nischal <contact@nischalneupane.name.np>
2023-02-02 11:07:32 +0545

    Added functions

--- Library_Management_System.py
+++ Library_Management_System.py
@@ -1,19 +1,35 @@
 class Library:
-    books_names = ["Nepali","English","Maths","Science", "Social"]
+    books_names = {"Nepali","English","Maths","Science", "Social"}
+    total_book_names = ["Nepali","English","Maths","Science", "Social"]
     
     def __init__(self):
         pass
-
+    
     def list_of_books(self):
         bnames = '\n'.join(self.books_names)
         print(bnames)
 
     def get_books(self):
-        pass
+        self.list_of_books()
+        print("\n")
+        userchoice = input("Enter the name of book you want:\n").title()
+        if userchoice in self.books_names:
+            print ("Take the book")
+            self.books_names.discard(userchoice)
+            print(self.books_names)
+        else:
+            print("Sorry!!Book is not available.")
         
 
     def return_books(self):
-        pass
+        userchoice = input("Enter the name of the book you want to return").title()
+        if userchoice in self.total_book_names :
+            self.books_names.add(self.userchoice)
+            print(self.books_names)
+        else :
+            print("The name of book was invalid")
+            
+
 
 if __name__ == "__main__":
     l = Library()
@@ -22,7 +38,7 @@ if __name__ == "__main__":
             userchoice = input("""Enter
             L - List of books.
             G - Get books.
-            R - Return books.""").capitalize()
+            R - Return books.:-\n""").capitalize()
             if userchoice == 'L':
                 l.list_of_books()
             if userchoice == 'G':
@@ -32,4 +48,12 @@ if __name__ == "__main__":
            
 
         except:
-            exit()
+            ValueError()
+        
+        finally:
+            userchoice = input("""Enter S - stay 
+Any other key - Exit""").capitalize()
+            if userchoice == 'S':
+                continue
+            else:
+                exit()
